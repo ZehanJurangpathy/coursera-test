118cae3eb258f907b886a34232b9925dbf30ed41
Zehan <jurangpathyz@something.com>
2017-01-23 12:55:07 +0530

    commit

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -4,17 +4,41 @@
   angular.module('NarrowItDownApp', [])
   .controller('NarrowItDownController', NarrowItDownController)
   .service('MenuSearchService', MenuSearchService)
-  .directive('foundItems', foundItems);
+  .directive('foundItems', FoundItems);
+
+  //foundItems directive
+  function FoundItems(){
+    var ddo = {
+      templateUrl : 'foundItems.html'
+    };
+
+    return ddo;
+  };
 
   NarrowItDownController.$inject = ['MenuSearchService']
   function NarrowItDownController(MenuSearchService){
     var narrow = this;
     //holds the searchTerm
     narrow.searchTerm = '';
-    narrow.found = '';
+    narrow.found = [];
     //click event
     narrow.narrowItDown = function () {
-      narrow.found = MenuSearchService.getMatchedMenuItems(narrow.searchTerm);
+      var promise = MenuSearchService.getMatchedMenuItems();
+
+      promise.then(function(result){
+        var foundItems = result.data.menu_items;
+        console.log(foundItems.length);
+        //loop through and find matching description
+        for (var i = 0; i < foundItems.length; i++) {
+          //console.log(foundItems.menu_items[i].description);
+          if (!foundItems[i].description.includes(narrow.searchTerm)) {
+            //if not found remove from the array
+            foundItems.splice(i, 1);
+          }
+        }
+        //assign to found
+        narrow.found = foundItems;
+      });
     };
   };
 
@@ -22,34 +46,11 @@
   function MenuSearchService($http){
     var service = this;
 
-    service.getMatchedMenuItems = function(searchTerm){
-      console.log(searchTerm);
-      var response = $http({
+    service.getMatchedMenuItems = function(){
+      return $http({
         method : 'GET',
         url : ("http://davids-restaurant.herokuapp.com/menu_items.json")
-      }).then(function(result){
-        var foundItems = result.data.menu_items;
-        //loop through and find matching description
-        for (var i = 0; i < foundItems.length; i++) {
-          //console.log(foundItems.menu_items[i].description);
-          if (!foundItems[i].description.includes(searchTerm)) {
-            //if not found remove from the array
-            foundItems.splice(i, 1);
-          }
-        }
-        console.log(foundItems.length);
-        //return found items
-        return foundItems;
       });
     };
-  };
-
-  //foundItems directive
-  function foundItems(){
-    var ddo = {
-      templateUrl : foundItems.html
-    };
-
-    return ddo;
   };
 })();
